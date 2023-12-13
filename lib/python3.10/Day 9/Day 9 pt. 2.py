1577505caf8c5aaf1b9cd6c905e633cd5ba84b3d
JP Lyon <japylyon@gmail.com>
2023-12-12 19:19:58 -0600

    Day 9 solutions

---
+++ lib/python3.10/Day 9/Day 9 pt. 2.py
@@ -0,0 +1,32 @@
+sensor_values = open('Sensor Values', 'r')
+lst = []
+total = 1
+for line in sensor_values.readlines():
+    word = line.strip()
+    temp_list = []
+    for char in word.split():
+        temp_list.append(int(char))
+    lst.append(temp_list)
+
+
+def derivate_list(lst) -> list[int]:
+    i = 0
+    new_lst = []
+    while i < len(lst) - 1:
+        new_lst.append(lst[i + 1] - lst[i])
+        i += 1
+    return new_lst
+
+
+def change (lst) -> int:
+    if lst == [0] * len(lst):
+        return 0
+    else:
+        return lst[0] - change(derivate_list(lst))
+
+
+print(lst)
+total = 0
+for set in lst:
+    total += change(set)
+print(total)
